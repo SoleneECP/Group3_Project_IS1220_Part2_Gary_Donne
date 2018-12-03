2e07e9e87580593545ff5c64b396eba20d696430
LucieEcp <45289647+LucieEcp@users.noreply.github.com>
2018-12-03 10:02:53 +0100

    Update AverageMarkVisitor.java

--- myUberUsers/AverageMarkVisitor.java
+++ myUberUsers/AverageMarkVisitor.java
@@ -1,5 +1,16 @@
 package myUberUsers;
 
-public class AverageMarkVisitor {
+import java.util.Map.Entry;
 
+public class AverageMarkVisitor implements DriversOperationVisitor {
+	@Override
+	public double visit(Driver driver) {
+		double sumOfIterations = 0;
+		double sumOfMarks= 0;
+		for ( Entry<Integer, Integer> entry: driver.getMarks().entrySet()) {
+			sumOfIterations += entry.getValue();
+			sumOfMarks+=entry.getKey()*entry.getValue();
+		}
+		return (double) sumOfMarks/sumOfIterations;
+	}
 }
